b7d444e61cee11563551d7be60fa1e907bc9b96b
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-01-29 19:39:43 +0530

    Add files via upload

---
+++ Diabetesapp.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('picklediabetes.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('DiabetesIndex.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = int(prediction[0])
+    if (output ==0):
+        return render_template('DiabetesIndex.html', prediction_text='Person is Suffering with diabetes')
+    else:
+        return render_template('DiabetesIndex.html', prediction_text='Person is not Suffering from diabetes')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
